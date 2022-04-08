6776d64a49391588f7b3b4393692e01cecb63cff
Maksym Shakhbazov <max_shakh@yahoo.com>
2022-04-08 18:13:57 -0500

    Coverage threshold changed to 0%

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
     ],
   coverageThreshold: {
     global: {
-      branches: -10,
-      functions: -10,
-      lines: -10,
-      statements: -10,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
     }
   }
 };
